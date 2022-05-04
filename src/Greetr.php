a8466b619610d71210a5dc108ece516ce1fd9e11
Zaw Htet Naing <zawhtetnaing@nexlabs.co>
2022-05-04 15:48:25 +0630

    cg msg

--- src/Greetr.php
+++ src/Greetr.php
@@ -6,6 +6,6 @@ class Greetr
 {
     public function greet(String $sName)
     {
-        return 'Hi ' . $sName . '! How are you doing today? U fine?';
+        return 'Hi ' . $sName . '! How are you doing today? U fine? this is v1.0.0';
     }
 }
